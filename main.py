ac89c613faaef8c4cec320d973fa064f85148411
svkkkurai <sakurai.pnwz@gmail.com>
2025-02-17 15:21:38 +0200

    update

--- main.py
+++ main.py
@@ -7,21 +7,16 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 import config
 
-# Settings for Spotify
 SPOTIFY_CLIENT_ID = config.SPOTIFY_CLIENT_ID
 SPOTIFY_CLIENT_SECRET = config.SPOTIFY_CLIENT_SECRET
 SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'
 SPOTIFY_SCOPE = 'user-read-playback-state'
 
-# Settings for Telegram
 API_ID = config.TG_API_ID
 API_HASH = config.TG_API_HASH
-SESSION_NAME = 'session'  # Имя сессии Telethon
+SESSION_NAME = 'session'
 
-# Telethon init
 client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
-
-# Spotify Client init
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                                                client_secret=SPOTIFY_CLIENT_SECRET,
                                                redirect_uri=SPOTIFY_REDIRECT_URI,
@@ -29,41 +24,47 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
 
 async def update_bio_with_track():
     async with client:
-        me = await client.get_me()  # Получаем основную информацию о пользователе
-        full_user = await client(GetFullUserRequest(me.id))  # Получаем полную информацию о пользователе
-        user_description = full_user.full_user.about or ''  # Сохраняем текущее описание профиля пользователя
+        me = await client.get_me()
+        full_user = await client(GetFullUserRequest(me.id))
+        original_bio = (full_user.full_user.about or '')[:70]
 
         while True:
             current_track = sp.current_playback()
 
-            if current_track and current_track['is_playing']:
-                track_name = current_track['item']['name']
-                artist_name = current_track['item']['artists'][0]['name']
-                progress_ms = current_track['progress_ms']
-                duration_ms = current_track['item']['duration_ms']
+            if current_track and current_track.get('is_playing'):
+                track_info = current_track.get('item')
+                if track_info:
+                    track_name = track_info.get('name', 'Unknown')
+                    artist_name = track_info['artists'][0].get('name', 'Unknown')
 
-                # Преобразование миллисекунд в минуты и секунды
-                progress_time = f"{progress_ms // 60000}:{str((progress_ms // 1000) % 60).zfill(2)}"
-                duration_time = f"{duration_ms // 60000}:{str((duration_ms // 1000) % 60).zfill(2)}"
+                    progress_ms = current_track.get('progress_ms', 0)
+                    duration_ms = track_info.get('duration_ms', 1)
 
-                bio_text = f"Now playing: {artist_name} - {track_name} ({progress_time} of {duration_time})"
+                    progress_time = f"{progress_ms // 60000}:{(progress_ms // 1000) % 60:02d}"
+                    duration_time = f"{duration_ms // 60000}:{(duration_ms // 1000) % 60:02d}"
 
-                # Если длина bio_text превышает 70 символов, обрезаем название трека
-                max_track_name_length = 70 - len(f"Now playing: {artist_name} -  ({progress_time} of {duration_time})")
-                if len(bio_text) > 70:
-                    track_name = track_name[:max_track_name_length - 3] + "..."
-                    bio_text = f"Now playing: {artist_name} - {track_name} ({progress_time} of {duration_time})"
+                    base_text = f"Слушает Spotify: {artist_name} - "
+                    time_text = f" ({progress_time} из {duration_time})"
+                    max_track_length = 70 - len(base_text) - len(time_text) # Change 70 to 140, if you have Telegram Premium.
+
+                    if len(track_name) > max_track_length:
+                        track_name = track_name[:max_track_length - 3] + "..."
+
+                    bio_text = base_text + track_name + time_text
             else:
-                bio_text = user_description[:70]  # Обрезаем описание пользователя, если нужно
+                full_user = await client(GetFullUserRequest(me.id))
+                current_bio = full_user.full_user.about or ''
+                
+                if current_bio != original_bio and not current_bio.startswith("Слушает Spotify"):
+                    original_bio = current_bio[:70]
+                
+                bio_text = original_bio
 
+            if full_user.full_user.about != bio_text:
+                await client(UpdateProfileRequest(about=bio_text))
+                print(f'Обновлено био: {bio_text}')
 
-            # Update bio in Telegram
-            await client(UpdateProfileRequest(about=bio_text))
-            print(f'Spotify - Now playing: {artist_name} - {track_name} ({progress_time} из {duration_time})')
-
-            # Await before update track
-            await asyncio.sleep(30)
-
-# Run bot
+            await asyncio.sleep(15)
+            
 with client:
     client.loop.run_until_complete(update_bio_with_track())
